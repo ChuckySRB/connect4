ce435e30f3df6ba1a0ba3a548e83ab3f5bb8dc83
ChuckySRB <chucky.srb@gmail.com>
2023-12-01 17:27:34 +0100

    Idemo do pobede

--- connect4-model/src/main/java/com/mozzartbet/hackaton/connect4/poluprovodnici/Bot.java
+++ connect4-model/src/main/java/com/mozzartbet/hackaton/connect4/poluprovodnici/Bot.java
@@ -113,6 +113,118 @@ public class Bot extends Player {
             return miniMax*eval;
         }
 
+        public float evaluateChain(int currentPlayer){
+
+            float eval = (float) 0;
+            int boardMatrix [][]= gameBoard.getBoard();
+            int column = gameBoard.getLastMove().getCol();
+            int row = gameBoard.getLastMove().getRow();
+
+            int num = 0;
+            int factor = 0;
+            // gledaj horizont
+            for (int cColumn = column-1; cColumn > 0; cColumn--){
+                if(boardMatrix[row][cColumn] == currentPlayer){
+                    num++;
+                }
+                else if (boardMatrix[row][cColumn] == 0){
+                    factor++;
+                    break;
+                }
+                else{
+                    break;
+                }
+            }
+            for (int cColumn = column+1; cColumn < GameConsts.COLUMNS; cColumn++){
+                if(boardMatrix[row][cColumn] == currentPlayer){
+                    num++;
+                }
+                else if (boardMatrix[row][cColumn] == 0){
+                    factor++;
+                    break;
+                }
+                else{
+                    break;
+                }
+            }
+
+            num *=factor;
+
+            num = 0;
+            factor = 0;
+            // gledaj horizont
+            for (int cColumn = column-1, cRow = row-1; cColumn > 0 && cRow > 0; cColumn--, cRow--){
+                if(boardMatrix[cRow][cColumn] == currentPlayer){
+                    num++;
+                }
+                else if (boardMatrix[cRow][cColumn] == 0){
+                    factor++;
+                    break;
+                }
+                else{
+                    break;
+                }
+            }
+            for (int cColumn = column+1, cRow = row+1; cColumn < GameConsts.COLUMNS && cRow < GameConsts.ROWS; cColumn++, cRow++) {
+                if (boardMatrix[cRow][cColumn] == currentPlayer) {
+                    num++;
+                } else if (boardMatrix[cRow][cColumn] == 0) {
+                    factor++;
+                    break;
+                } else {
+                    break;
+                }
+
+            }
+                num *=factor;
+
+            eval += num/6;
+
+            num = 0;
+            factor = 0;
+            // gledaj horizont
+            for (int cColumn = column+1, cRow = row-1; cColumn < GameConsts.COLUMNS && cRow > 0; cColumn++, cRow--){
+                if(boardMatrix[cRow][cColumn] == currentPlayer){
+                    num++;
+                }
+                else if (boardMatrix[cRow][cColumn] == 0){
+                    factor++;
+                    break;
+                }
+                else{
+                    break;
+                }
+            }
+            for (int cColumn = column-1, cRow = row+1; cColumn > 0 && cRow < GameConsts.ROWS; cColumn--, cRow++){
+                if(boardMatrix[cRow][cColumn] == currentPlayer){
+                    num++;
+                }
+                else if (boardMatrix[cRow][cColumn] == 0){
+                    factor++;
+                    break;
+                }
+                else{
+                    break;
+                }
+            }
+
+
+                num *=factor;
+
+                eval += num/6;
+
+                // gledaj vertikala
+
+                // gledaj /
+
+                // gledaj \
+
+                eval = Math.min(1.0f, Math.max(-1.0f, eval/3));
+
+                return miniMax*eval;
+
+            }
+
         public float gainLogic(int currentPlayer, GameBoard board) {
             // Check for winning states
 
@@ -125,12 +237,15 @@ public class Bot extends Player {
             // Check for the number of winning positions for both players
             float middleBoxEvaluation = (float)(evaluateBoxInner(currentPlayer) * 0.2  + evaluateBoxOuter(currentPlayer) * 0.1);
 
+            float chainEvaluation = (float)(evaluateChain(currentPlayer)*0.4);
+
             //
             float levelEvaluation = (float)(evaluateLevel()*0.1);
             // Combine factors to get an overall evaluation
             float evaluation = 0;
             evaluation += middleBoxEvaluation;
             evaluation += levelEvaluation;
+            evaluation += chainEvaluation;
 
             // Normalize the evaluation to be between -1 and 1
             return Math.min(1.0f, Math.max(-1.0f, evaluation));
@@ -194,7 +309,10 @@ public class Bot extends Player {
         float gain =root.calculateGain();
         move = root.getNextMove();
         gameBoard.placeCounter(move, 3-gameBoard.getLastCounterPlaced());
-    }
+        if (gameBoard.isGameOver()){
+            gameBoard.reset();
+
+        }    }
 
     @Override
     public void stop() {
